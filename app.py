4ac31763bd1c28057b1a0b3832d69a7f4a60fba2
Neil Peterson <nepeters@microsoft.com>
2024-12-13 22:45:17 -0800

    Added Cosmos DN Demo

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from azure.cosmos import CosmosClient, exceptions, PartitionKey
+import os
 
 app = Flask(__name__)
+
+# Initialize Cosmos DB client
+COSMOS_ENDPOINT = os.getenv('COSMOS_ENDPOINT')
+COSMOS_KEY = os.getenv('COSMOS_KEY')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+CONTAINER_NAME = os.getenv('CONTAINER_NAME')
+
+client = CosmosClient(COSMOS_ENDPOINT, COSMOS_KEY)
+database = client.create_database_if_not_exists(id=DATABASE_NAME)
+container = database.create_container_if_not_exists(
+    id=CONTAINER_NAME,
+    partition_key=PartitionKey(path="/id"),
+    offer_throughput=400
+)
 
 @app.route('/sum', methods=['POST'])
 def sum():
@@ -12,6 +28,21 @@ def sum():
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/read', methods=['GET'])
+def query_cosmos():
+    query = "SELECT * FROM c"
+    items = list(container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
+    return jsonify(items)
+
+@app.route('/write', methods=['POST'])
+def write_cosmos():
+    data = request.json
+    container.create_item(body=data)
+    return jsonify({'status': 'Item created successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
